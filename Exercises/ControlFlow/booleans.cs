55595e6b83ebe882c8eefe2526db7931a5afae87
Ben Malberg <bmalberg@protonmail.com>
2026-01-06 19:46:06 -0800

    Module 3: ToLowwer/ToTrim, inequality Boolean expressions examples in ControlFlow/booleans.cs

--- Exercises/ControlFlow/booleans.cs
+++ Exercises/ControlFlow/booleans.cs
@@ -5,7 +5,6 @@ namespace Exercises.ControlFlow
         public static void Run()
         {
             // equality operator
-
             Console.WriteLine($"The result of (\"a\" == \"A\") is {("a" == "A")}"); // using the escape sequence \" to write a double quote as a string
             Console.WriteLine("a" == "A");
 
@@ -13,6 +12,29 @@ namespace Exercises.ControlFlow
             string myValue = "ABC";
             Console.WriteLine($"{myValue} == ABC");
 
+            Console.WriteLine();
+            // using .ToLower().Trim();
+            string value1 = " Apple ";
+            string value2 = value1.ToLower().Trim();
+            Console.WriteLine($"|{value1}| -> |{value2}|");
+            
+            Console.WriteLine();
+            //inequality
+            value1 = "A";
+            value2 = "a";
+            Console.WriteLine($"{value1} != {value2}: {value1 != value2}");
+
+            Console.WriteLine();
+            //comparison operator >= using dice roll
+            Random roll = new Random();
+            int roll1 = roll.Next(1, 7);
+            Console.WriteLine(roll1);
+            int roll2 = roll.Next(1, 7);
+            Console.WriteLine(roll2);
+            Console.WriteLine($"{roll1} >= {roll2}: {roll1 >= roll2}");
+            roll1 = roll.Next(1, 7);
+            roll2 = roll.Next(1, 7);
+            Console.WriteLine($"{roll1} >= {roll2}: {roll1 >= roll.Next(1, 7)}");
         }
 
     }
