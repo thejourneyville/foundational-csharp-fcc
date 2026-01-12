7a6bffcd4301456fb4be2e92a37bf1dbb8c5a9a1
Ben Malberg <bmalberg@protonmail.com>
2026-01-12 13:10:34 -0800

    switch-case challenge - sku identification

--- Exercises/ControlFlow/switch-statements.cs
+++ Exercises/ControlFlow/switch-statements.cs
@@ -4,44 +4,67 @@ namespace Exercises.ControlFlow
     {
         public static void Run()
         {
-            int employeeLevel = 200;
-            string employeeName = "John Smith";
+            // SKU = Stock Keeping Unit
+            // SKU value format: <product #>-<2-letter color code>-<size code>
+            string sku = "02-BL-S";
 
-            string title = "";
+            string[] product = sku.Split('-');
 
-            switch (employeeLevel)
+            string type = "";
+            string color = "";
+            string size = "";
+
+            foreach (string section in product)
             {
-                case 100:
-                case 200:
-                    title = "Senior Associate";
-                    break;
-                
-                case 300:
-                    title = "Manager";
-                    break;
+                switch (section)
+                {
+                    case "01":
+                        type = "Sweat shirt";
+                        break;
 
-                case 400:
-                    title = "Senior Manager";
-                    break;
-                default:
-                    title = "Associate";
-                    break;
+                    case "02":
+                        type = "T-Shirt";
+                        break;
+                    
+                    case "03":
+                        type = "Sweat pants";
+                        break;
+                    
+                    case "BL":
+                        color = "Black";
+                        break;
+
+                    case "MN":
+                        color = "Maroon";
+                        break;
+
+                    case "S":
+                        size = "Small";
+                        break;
+
+                    case "M":
+                        size = "Medium";
+                        break;
+                    
+                    case "L":
+                        size = "Large";
+                        break;
+
+                    default:
+                        // checks each section from the loop, 
+                        // if the section is empty and the variable is empty, will populate in order
+                        if (type == "")
+                            type = "Other";
+                        if (color == "")
+                            color = "White";
+                        if (size == "")
+                            size = "One Size Fits All";
+                        break;
+ 
+                }
             }
 
-            Console.WriteLine($"{employeeName}: {title}");
-
-            int someNumber = 6;
-
-            switch (someNumber) {
-                case 5:
-                    Console.WriteLine("5");
-                    break;
-
-                case 7:
-                    Console.WriteLine("7");
-                    break;
-
-            }
+            Console.WriteLine($"Product: {size} {color} {type}");
         }
 
     }
