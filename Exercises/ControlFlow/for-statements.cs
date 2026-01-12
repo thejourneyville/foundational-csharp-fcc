755db620a49d89c8593f0db83c73496333a82a9d
Ben Malberg <bmalberg@protonmail.com>
2026-01-12 14:32:38 -0800

    for blocks with breaks and conditions

--- Exercises/ControlFlow/for-statements.cs
+++ Exercises/ControlFlow/for-statements.cs
@@ -4,7 +4,12 @@ namespace Exercises.ControlFlow
     {
         public static void Run()
         {
-            
+            // for (initializer; condition; iterator) { body }
+            for (int i = 0; i < 10; i += 3)
+            {
+                Console.WriteLine(i);
+                if (i == 6) break;
+            }
         }
 
     }
