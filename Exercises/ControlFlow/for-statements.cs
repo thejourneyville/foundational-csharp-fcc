83eab7e872deded8b8ec387b343f54e531d08108
Ben Malberg <bmalberg@protonmail.com>
2026-01-13 14:28:41 -0800

    for loops, changing value with index

--- Exercises/ControlFlow/for-statements.cs
+++ Exercises/ControlFlow/for-statements.cs
@@ -5,11 +5,26 @@ namespace Exercises.ControlFlow
         public static void Run()
         {
             // for (initializer; condition; iterator) { body }
-            for (int i = 0; i < 10; i += 3)
+            // for (int i = 0; i < 10; i += 3)
+            // {
+            //     Console.WriteLine(i);
+            //     if (i == 6) break;
+            // }
+
+            string[] names = new string[4];
+            names[0] = "a";
+            names[1] = "b";
+            names[2] = "c";
+            names[3] = "d";
+            // names[4] = "e";
+            //names[5] = "f";
+
+            for (int i = names.Length - 1; i >= 0; i--)
             {
-                Console.WriteLine(i);
-                if (i == 6) break;
+                if (names[i] == "b") names[i] = "benny";
             }
+
+            foreach (var name in names) Console.WriteLine($"{name}");
         }
 
     }
