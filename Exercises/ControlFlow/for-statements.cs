432fc33d08ab95ec4525c42724e13bc5d2538dac
Ben Malberg <bmalberg@protonmail.com>
2026-01-14 13:04:42 -0800

    for loop - fizzbuzz challenge

--- Exercises/ControlFlow/for-statements.cs
+++ Exercises/ControlFlow/for-statements.cs
@@ -4,27 +4,23 @@ namespace Exercises.ControlFlow
     {
         public static void Run()
         {
-            // for (initializer; condition; iterator) { body }
-            // for (int i = 0; i < 10; i += 3)
-            // {
-            //     Console.WriteLine(i);
-            //     if (i == 6) break;
-            // }
+            //fizzbuzz
 
-            string[] names = new string[4];
-            names[0] = "a";
-            names[1] = "b";
-            names[2] = "c";
-            names[3] = "d";
-            // names[4] = "e";
-            //names[5] = "f";
+            /*
+            Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
+            When the current value is divisible by 3, print the term Fizz next to the number.
+            When the current value is divisible by 5, print the term Buzz next to the number.
+            When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
+            */
 
-            for (int i = names.Length - 1; i >= 0; i--)
+            for (int i = 1; i <= 100; i++)
             {
-                if (names[i] == "b") names[i] = "benny";
-            }
+                if (i % 3 == 0 && !(i % 5 == 0)) Console.WriteLine($"{i} - Fizz");
+                else if (i % 5 == 0 && !(i % 3 == 0)) Console.WriteLine($"{i} - Buzz");
+                else if (i % 3 == 0 && i % 5 == 0) Console.WriteLine($"{i} - FizzBuzz");
+                else Console.WriteLine(i);
 
-            foreach (var name in names) Console.WriteLine($"{name}");
+            }
         }
 
     }
