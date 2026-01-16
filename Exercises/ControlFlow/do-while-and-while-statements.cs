5efb28ca494a5c74f349b870dc714d582fa557e6
Ben Malberg <bmalberg@protonmail.com>
2026-01-15 19:14:22 -0800

    do-while and while loop practice using Random

---
+++ Exercises/ControlFlow/do-while-and-while-statements.cs
@@ -0,0 +1,28 @@
+namespace Exercises.ControlFlow
+{
+    public static class DoWhileStatements
+    {
+        public static void Run()
+        {
+            Random random = new Random();
+            int current = random.Next(1, 11);
+
+            do
+            {
+                current = random.Next(1, 11);
+
+                if (current >= 8) continue;
+
+                Console.WriteLine(current);
+            } while (current != 7);
+
+            // while (current >= 3)
+            // {
+            //     Console.WriteLine(current);
+            //     current = random.Next(1, 11);
+            // }
+            // Console.WriteLine($"Last number: {current}");
+        }
+    }
+}
+
