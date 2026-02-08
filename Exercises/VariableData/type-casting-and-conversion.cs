f3db6d330acc3345eb9dac48b448074d607fdfeb
Ben Malberg <bmalberg@protonmail.com>
2026-02-08 07:37:51 -0800

    narrowing conversions with casting

--- Exercises/VariableData/type-casting-and-conversion.cs
+++ Exercises/VariableData/type-casting-and-conversion.cs
@@ -18,8 +18,18 @@ namespace Exercises.VariableData
             decimal myDecimal = 3.14m;
             Console.WriteLine($"decimal: {myDecimal}");
 
+            // cast - a narrowing conversion
             int myInt = (int)myDecimal;
             Console.WriteLine($"int: {myInt}");
+
+            // another narrowing conversion from decimal to float
+            decimal myDecimal1 = 1.12345678123456789m;
+            float myFloat1 = (float)myDecimal1;
+
+            Console.WriteLine($"myDecimal1:     {myDecimal1}");
+            Console.WriteLine($"myFloat1:       {myFloat1}");
+
+
 
         }
     }
