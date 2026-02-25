229a5355c3907c97ef672f3cf89744715d715d32
Ben Malberg <bmalberg@protonmail.com>
2026-02-24 19:26:40 -0800

    using Array.Clear(arrayName, starting index, quantity of elements)

--- Exercises/VariableData/arrays-sort-and-reverse.cs
+++ Exercises/VariableData/arrays-sort-and-reverse.cs
@@ -20,6 +20,34 @@ namespace Exercises.VariableData
             {
                 Console.WriteLine($"-- {pallet}");
             }
-        }   
+
+            Console.WriteLine("");
+            Console.WriteLine("Using Array method Clear...");
+            Array.Clear(pallets, 2, 1);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Trying to access a cleared index in an array...");
+            Console.WriteLine($"pallets[2]: {pallets[2]}");
+
+            Console.WriteLine("");
+            Console.WriteLine("Getting length of array after using array method Clear...");
+            Console.WriteLine($"pallets array length: {pallets.Length}");
+
+            Console.WriteLine("");
+            Console.WriteLine("Verifying pallets cleaed index is available...");
+            try
+            {
+                Console.WriteLine($"pallets[2].ToLower(): {pallets[2].ToLower()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}");
+            }
+
+        }
     }
 }
