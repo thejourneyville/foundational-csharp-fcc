44bb192eff9f228ad1b77e57c0fcbb2a805e676c
Ben Malberg <bmalberg@protonmail.com>
2026-03-10 19:49:49 -0700

    challenge - reverse words solved

---
+++ Exercises/VariableData/challenge-reverse-words-in-sentence.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel;
+using System.Threading.Tasks.Dataflow;
+
+namespace Exercises.VariableData
+{
+    public static class ReverseWordsInSentence
+    {
+        public static void Run()
+        {
+            // Write the code necessary to reverse the letters of each word in place and display the result.
+
+            // In other words, don't just reverse every letter in the variable pangram. Instead, you need to reverse just the letters in each word, but print the reversed word in its original position in the message.
+
+            // Your code must produce the following output:
+
+            // Output
+            // ehT kciuq nworb xof spmuj revo eht yzal god
+
+            string pangram = "The quick brown fox jumps over the lazy dog.";
+
+            string[] words = pangram.Split(" ");
+            string output = "";
+            int index = 0;
+
+            foreach (string word in words)
+            {
+                if (word != null)
+                {
+                    char[] charWord = word.ToCharArray();
+                    Array.Reverse(charWord);
+                
+                    if (charWord != null)
+                    {
+                        output += string.Join("", charWord) + " ";
+                    }
+                    
+                    index++;
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
