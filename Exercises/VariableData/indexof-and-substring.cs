d4baaf6c9c3359f199f3b1e0e9bd947d7a164ebe
Ben Malberg <bmalberg@protonmail.com>
2026-04-09 20:12:49 -0700

    using IndexOf and Substring to capture and isolate part of the string

--- Exercises/VariableData/indexof-and-substring.cs
+++ Exercises/VariableData/indexof-and-substring.cs
@@ -7,18 +7,32 @@ namespace Exercises.VariableData
     {
         public static void Run()
         {
-            string message = "Find what is (inside the parentheses)";
+            string message1 = "Find what is (inside the parentheses)";
 
             string startingChar = "(";
             string endingChar = ")";
 
-            int openingPosition = message.IndexOf(startingChar) + 1;
-            int endingPosition = message.IndexOf(endingChar);
+            int openingPosition = message1.IndexOf(startingChar) + 1;
+            int endingPosition = message1.IndexOf(endingChar);
             int substringLength = endingPosition - openingPosition;
 
-            string output = message.Substring(openingPosition, substringLength);
+            string output = message1.Substring(openingPosition, substringLength);
 
             Console.WriteLine(output);
+
+            // ============
+
+            string message2 = "what is value <span>between the tags</span>?";
+
+            const string openSpan = "<span>";
+            const string closedSpan = "</span>";
+
+            int openingPosition1 = message2.IndexOf(openSpan) + openSpan.Length;
+            int endingPosition1 = message2.IndexOf(closedSpan);
+
+            Console.WriteLine($"{message2.Substring(openingPosition1, endingPosition1 - openingPosition1)}");
+            
+
         }
     }
 }
