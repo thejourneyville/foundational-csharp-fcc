636dca6472d73251f26cfd9d12c0083d931100d4
Ben Malberg <bmalberg@protonmail.com>
2026-04-28 19:55:23 -0700

    indexOfAny, Substring with while loop to find all occurances of strings inside a variety of characters

--- Exercises/VariableData/indexofany.cs
+++ Exercises/VariableData/indexofany.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Threading.Tasks.Dataflow;
 
 namespace Exercises.VariableData
@@ -7,22 +8,61 @@ namespace Exercises.VariableData
     {
         public static void Run()
         {
-            string message = "(What if) there are (more than) one (set of parentheses)?";
+            string message1 = "(What if) there are (more than) one (set of parentheses)?";
 
-            while (true) {
-                int openingIdx = message.IndexOf('(');
+            while (true)
+            {
+                int openingIdx = message1.IndexOf('(');
                 if (openingIdx == -1) break;
-                
-                int closingIdx = message.IndexOf(')');
+
+                int closingIdx = message1.IndexOf(')');
 
                 int firstCharIdx = openingIdx + 1;
                 int length = closingIdx - firstCharIdx;
-                
-                Console.WriteLine(message.Substring(firstCharIdx, length));
-                
-                message = message.Substring(closingIdx+1);
+
+                Console.WriteLine(message1.Substring(firstCharIdx, length));
+
+                message1 = message1.Substring(closingIdx + 1);
             }
 
+            Console.WriteLine("==============================");
+
+            string message2 = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+            int openingIdx2 = 0;
+
+
+            while (openingIdx2 >= 0)
+            {
+                char[] possibleSymbols = { '(', '[', '{' };
+
+                openingIdx2 = message2.IndexOfAny(possibleSymbols);
+                if (openingIdx2 == -1) break;
+                
+                string openingSymbol = message2.Substring(openingIdx2, 1);
+                char closingSymbol = ' ';
+
+                switch (openingSymbol)
+                {
+                    case "(":
+                        closingSymbol = ')';
+                        break;
+
+                    case "[":
+                        closingSymbol = ']';
+                        break;
+
+                    case "{":
+                        closingSymbol = '}';
+                        break;
+                }
+
+                int closingIdx2 = message2.IndexOf(closingSymbol);
+                int stringLength = closingIdx2 - openingIdx2;
+                Console.WriteLine(message2.Substring(openingIdx2 + 1, stringLength - 1));
+
+                message2 = message2.Substring(closingIdx2);
+
+            }
         }
     }
 }
